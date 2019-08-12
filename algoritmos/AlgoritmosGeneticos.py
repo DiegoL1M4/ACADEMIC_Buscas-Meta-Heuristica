e16c05807e6c01a50687d8035f46aed37e56d6c2
Diego Lima <diego-dlp@hotmail.com>
2019-08-12 06:22:42 -0300

    Atualização

--- algoritmos/AlgoritmosGeneticos.py
+++ algoritmos/AlgoritmosGeneticos.py
@@ -8,7 +8,7 @@ class AlgoritmosGeneticos(object):
     def __init__(self, game):
         self.game = game
 
-    def result(self, limit, tamPopulation, E, R, M, mask):
+    def result(self, limit, tamPopulation, E, R, M):
         # Time
         start = time.time()
 
@@ -25,13 +25,15 @@ class AlgoritmosGeneticos(object):
             evaluations = []
             for individual in population:
                 evaluations.append( self.game.eval(individual) )
-
+            
+            # Selecting the best of the current population
             res = population[ evaluations.index( min(evaluations) ) ]
-
+            
+            # Stop condition
             if( min(evaluations) == 0 ):
                 break
-            # As evaluations servem só pra ter saber se convergiu?
-
+            
+            # New population
             newPopulation = []
             join = []
 
@@ -63,7 +65,6 @@ class AlgoritmosGeneticos(object):
                 newIndividual = newPopulation[f2][0:div]
                 newIndividual.extend( newPopulation[f1][div:8] )
                 join.append( newIndividual )
-            #Duvida: uso pra reprodução sempre a lista atualizada da nova população?
 
             # Mutation
             iterations = int(tamPopulation * M)
@@ -71,7 +72,7 @@ class AlgoritmosGeneticos(object):
                 indexRan = random.randrange(0, len(newPopulation))
                 copy = newPopulation[indexRan][:]
 
-                selectColumn = random.randrange(0, len(mask))
+                selectColumn = random.randrange(0, 8)
 
                 copy[selectColumn] = self.game.getValue()
                 join.append(copy)
@@ -87,103 +88,3 @@ class AlgoritmosGeneticos(object):
         print("Total de execuções: " + str(round + 1))
 
         return res
-
-    def result2(self, limit, tamPopulation, E, R, M, mask):
-        # Time
-        start = time.time()
-
-        res = []
-
-        # Create a population
-        population = []
-        for k in range(tamPopulation):
-            population.append( self.game.create() )
-
-        # Iterations with limit
-        for round in range(limit):
-            # Evaluation
-            evaluations = []
-            for individual in population:
-                evaluations.append( self.game.eval(individual) )
-
-            if( min(evaluations) == 0 ):
-                res = population[ evaluations.index( min(evaluations) ) ]
-                break
-            # As evaluations servem só pra ter saber se convergiu?
-
-            newPopulation = []
-            join = []
-
-            # Election
-            iterations = int(tamPopulation * E)
-            for k in range(iterations):
-                select1 = random.randrange(0, len(population))
-                select2 = random.randrange(0, len(population))
-                                
-                if(self.game.eval( population[select1] ) < self.game.eval( population[select2] )):
-                    newPopulation.append( population[select1] )
-                    population.remove( population[select1] )
-                else:
-                    newPopulation.append( population[select2] )
-                    population.remove( population[select2] )
-                
-            # Reproduction
-            iterations = int( (tamPopulation * R) / 2 )
-            for k in range(iterations):
-                f1 = random.randrange(0, len(newPopulation))
-                f2 = random.randrange(0, len(newPopulation))
-                newIndividual = []
-                for k in range(len(mask)):
-                    if(mask[k] == 0):
-                        newIndividual.append(newPopulation[f1][k])
-                    else:
-                        newIndividual.append(newPopulation[f2][k])
-                join.append( newIndividual )
-                newIndividual = []
-                for k in range(len(mask)):
-                    if(mask[k] == 0):
-                        newIndividual.append(newPopulation[f2][k])
-                    else:
-                        newIndividual.append(newPopulation[f1][k])
-                join.append( newIndividual )
-            #Duvida: uso pra reprodução sempre a lista atualizada da nova população?
-
-            # Mutation
-            iterations = int(tamPopulation * M)
-            for k in range(iterations):
-                indexRan = random.randrange(0, len(newPopulation))
-                copy = newPopulation[indexRan][:]
-
-                selectColumn1 = random.randrange(0, len(mask))
-                selectColumn2 = random.randrange(0, len(mask))
-
-                aux = copy[selectColumn1]
-                copy[selectColumn1] = copy[selectColumn2]
-                copy[selectColumn2] = aux
-
-                join.append(copy)
-
-            
-            newPopulation.extend( join )
-            population = newPopulation
-
-        # Time
-        end = time.time()
-
-        print("\nTotal de excução: " + str(end - start))
-        print("Total de execuções: " + str(round + 1))
-
-        return res
-
-
-        
-        # Mutation
-        # iterations = int(tamPopulation * M)
-        # for k in range(iterations):
-        #     indexRan = random.randrange(0, len(newPopulation))
-        #     copy = newPopulation[indexRan][:]
-
-        #     selectColumn = random.randrange(0, len(mask))
-
-        #     copy[selectColumn] = self.game.getValue()
-        #     newPopulation.append(copy)
--- main.py
+++ main.py
@@ -5,57 +5,54 @@ from algoritmos import HillClimbing
 from algoritmos import SimulatedAnnealing
 from algoritmos import AlgoritmosGeneticos
 
-print('\nProblema Inicial:')
+#print('\nProblema Inicial:')
 problem = OitoRainhas.OitoRainhas()
-problem.show(problem.positions)
+#problem.show(problem.positions)
 result = []
 
-#choice = input()
-choice = 3
+choice = int(input("\nOpção: "))
+rangeInt = int(input("Range: "))
 
 # Main (Hill Climbing)
 if(choice == 1):
-    #solver = HillClimbing.HillClimbing(problem)
-    #result = solver.result( 100 )
+    print("\nHill Climbing")
     cont = 0
-    for k in range(100):
+    for k in range(rangeInt):
         problem = OitoRainhas.OitoRainhas()
         solver = HillClimbing.HillClimbing(problem)
         result = solver.result(100)
-        print( "Total de ataques: " + str(problem.eval(result)) )
+
         if (problem.eval(result) == 0):
             cont += 1
-    print( "Total de Acertos: " + str(cont) )
+    print( "\nEficiência: " + str( cont ) + '/' + str(rangeInt) )
 
-# Main (Simulated Annealing)
+# Main (Simulated Annealing) 
 elif(choice == 2):
+    print("\nSimulated Annealing")
     cont = 0
-    for k in range(1000):
+    for k in range(rangeInt):
         problem = OitoRainhas.OitoRainhas()
         solver = SimulatedAnnealing.SimulatedAnnealing(problem)
-        result = solver.result(500, 5000, 500, 50, 0.8)
-        print( "Total de ataques: " + str(k) )
-        if (problem.eval(result) != 0):
+        result = solver.result(500, 5000, 1000, 45, 0.8)
+
+        if (problem.eval(result) == 0):
             cont += 1
-    print( "Total de falhas: " + str(k+1) )
+    print( "\nEficiência: " + str( cont ) + '/' + str(rangeInt) )
 
 # Main (Algoritmos Genéticos)
 elif(choice == 3):
+    print("\nAlgoritmos Genéticos")
     cont = 0
-    for k in range(10):
+    for k in range(rangeInt):
+        problem = OitoRainhas.OitoRainhas()
         solver = AlgoritmosGeneticos.AlgoritmosGeneticos(problem)
-        result = solver.result(3000, 100, 0.5, 0.4, 0.1, [0,0,0,0,1,1,1,1])
+        result = solver.result(3000, 100, 0.4, 0.2, 0.4)
+
         if (problem.eval(result) == 0):
             cont += 1
-    print( "Total de ataques: " + str( cont ) )
+    print( "\nEficiência: " + str( cont ) + '/' + str(rangeInt))
 
-print( "Total de sucessos: " + str(problem.eval(result)) )
+print( "\nTotal de sucessos: " + str(problem.eval(result)) )
 
 # Contruct the board
-for i in range(8):
-    for j in range(8):
-        if(result[j] == i+1):
-            print(" 1", end="")
-        else:
-            print(" 0", end="")
-    print()
+problem.show(result)
